785ec10aa8b1ff5c824a1949bc3a02c857b11410
Dennis Henderson <dennis.henderson@baptisthealth.net>
2023-09-13 06:31:37 -0400

    added main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-00a9282ce3b5ddfb1"
+  instance_type = lookup(var.instance_type, terraform.workspace)
+}
